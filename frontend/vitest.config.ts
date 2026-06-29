20163d1b9abb59b1ea2cfca28ca3c5b4cc6b5abc
Omar Abdelwahab Hassan Ramdan <omarabdelwahab2005@gmail.com>
2026-06-30 00:23:02 +0200

    d

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import type { InlineConfig } from "vitest";
 
 export default defineConfig({
   plugins: [react()],
